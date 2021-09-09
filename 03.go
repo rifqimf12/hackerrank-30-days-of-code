880d6641a92bbbf74efe21468e643acb2b261c9f
rifqimf12 <rifqi12muhf@gmail.com>
2021-09-09 14:26:05 +0700

    add 03 solution

---
+++ 03.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	NTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(NTemp)
+	if n%2 == 0 && (n <= 5 || n > 20) {
+		fmt.Println("Not Weird")
+	} else {
+		fmt.Println("Weird")
+	}
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
